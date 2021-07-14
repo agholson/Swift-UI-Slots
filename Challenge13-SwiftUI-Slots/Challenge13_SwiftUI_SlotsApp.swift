24878eb2c2fc6be181a505fd2931c70090ef9fc6
ACGholson <myEmail@.com>
2021-07-13 21:15:23 -0400

    Initial Commit

---
+++ Challenge13-SwiftUI-Slots/Challenge13_SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge13_SwiftUI_SlotsApp.swift
+//  Challenge13-SwiftUI-Slots
+//
+//  Created by Shepherd on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge13_SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge13-SwiftUI-Slots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge13-SwiftUI-Slots
+//
+//  Created by Shepherd on 7/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
